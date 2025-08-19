1d102164907375afcd60f79e0eb6d2620ffb1bb6
Gamze <89346098+sgamzeo@users.noreply.github.com>
2025-08-19 17:51:24 +0300

    chore: configure Firebase for iOS & Android and update iOS deployment target to 15.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:firebase_challenge/core/route/router.dart';
 import 'package:firebase_challenge/core/route/routes.dart';
 import 'package:firebase_challenge/core/theme/theme_extensions.dart';
+import 'package:firebase_challenge/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:device_preview/device_preview.dart';
 import 'core/constants/dimens.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(
     DevicePreview(enabled: !kReleaseMode, builder: (context) => const MyApp()),
